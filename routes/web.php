a3acc3164e2d8dbdda31486c660b3a0645b8612f
sabirama <markcondesmail@gmail.com>
2025-07-21 12:43:50 +0800

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ Route::middleware('auth:admin')->group(function () {
         return view('admin.dashboard');
     })->name('dashboard');
 
+    Route::get('/users', [UserController::class, 'index'])->name('user');
+
+    Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+    Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+    Route::post('/user/mass-store', [UserController::class, 'massStore'])->name('user.mass-store');
+
+    Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
+
+
     Route::get('/api-keys', [ApiTokenController::class, 'index'])->name('api_tokens.index');
     Route::post('/api-keys', [ApiTokenController::class, 'store'])->name('api_tokens.store');
     Route::delete('/api-keys/{apiToken}', [ApiTokenController::class, 'destroy'])->name('api_tokens.destroy');
@@ -27,13 +36,4 @@ Route::middleware('guest')->group(function () {
 
     Route::get('/register', [AdminAuthController::class, 'showRegisterForm']);
     Route::post('/register', [AdminAuthController::class, 'register'])->name('register');
-
-    Route::get('/users', [UserController::class, 'index'])->name('user');
-
-    Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
-    Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
-    Route::post('/user/mass-store', [UserController::class, 'massStore'])->name('user.mass-store');
-
-    Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
-
 });
